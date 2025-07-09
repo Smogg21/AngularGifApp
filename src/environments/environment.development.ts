fa22c7291b0773c9f4298219bba54c25f6ed6bf8
Smogg21 <spazzaglia98@gmail.com>
2025-07-08 19:05:04 -0500

    Add Giphy service, mapper, and interfaces; configure app for HTTP client

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,6 +5,7 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   // ApiKeys
-
+  giphyApiKey: 'zzlaprMBuF2vlIlCTkDuMD421Sw3wxQR',
+  giphyUrl: 'https://api.giphy.com/v1',
   // URLS
 };
